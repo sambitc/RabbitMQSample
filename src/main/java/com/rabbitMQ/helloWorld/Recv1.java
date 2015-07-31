fe3756e6f6135f670e52757eee0dfe29a6e0441b
sambitchoudhury <sambitgudu@yahoo.co.in>
2015-07-31 16:23:40 +0530

    Initial commit

---
+++ src/main/java/com/rabbitMQ/helloWorld/Recv1.java
@@ -0,0 +1,34 @@
+package com.rabbitMQ.helloWorld;
+
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class Recv1 {
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] argv) throws java.io.IOException,
+			java.lang.InterruptedException, TimeoutException {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+
+		while (true) {
+			System.out.println("LOOP--------------");
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			System.out.println(" [x] Received '" + message + "'");
+		}
+	}
+}
